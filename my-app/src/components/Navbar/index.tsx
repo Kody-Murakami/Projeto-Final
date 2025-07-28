2db1c1cf989033a7c0cb14b36cd6bc6181c983dc
Breno PZ <d2023001951@unifei.edu.br>
2025-07-28 12:40:20 -0300

    feat: adiciona botao de navegacao mobile

---
+++ my-app/src/components/Navbar/index.tsx
@@ -0,0 +1,32 @@
+// src/components/Navbar.tsx
+import Link from 'next/link'; // ou 'react-router-dom' se for React puro
+import { useState } from 'react';
+import { Menu, X } from 'lucide-react';
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header className="bg-black text-white">
+  
+        {/* Botão hamburguer para mobile */}
+        <button
+          className="lg:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+          aria-label="Abrir menu"
+        >
+          {isOpen ? <X /> : <Menu />}
+        </button>
+      
+      {/* Menu de navegação mobile */}
+      {isOpen && (
+        <nav className="lg:hidden flex flex-col space-y-4 mt-4 px-4">
+          <Link href="#contato" onClick={() => setIsOpen(false)}>Contato</Link>
+          <Link href="/Sobre" onClick={() => setIsOpen(false)}>Sobre nós</Link>
+          <Link href="/Sobre" onClick={() => setIsOpen(false)}>Carrinho</Link>
+          <Link href="/Sobre" onClick={() => setIsOpen(false)}>Cadastre-se</Link>
+        </nav>
+      )}
+    </header>
+  );
+}
