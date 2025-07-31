70ccc1f3479462988e2e890145bb8e1ea571cea2
Breno PZ <d2023001951@unifei.edu.br>
2025-07-31 11:04:16 -0300

    feat: cria arquivo com os dados das pizzas

--- my-app/src/app/Cardapio/page.tsx
+++ my-app/src/app/Cardapio/page.tsx
@@ -3,82 +3,18 @@
 import Image from 'next/image' // Se estiver usando Next.js
 // Remova se for React puro
 import Link from 'next/link'
+import { pizzas } from '../../Data/pizzas';
 
 export default function Cardapio() {
-  const pizzas_tradicionais = [
-    {
-      nome: "Frango com Catupiry",
-      descricao: "Frango desfiado com catupiry cremoso e orégano.",
-      preco: "R$ 39,90",
-      imagem: "/imagens/cardapio/frango.jpeg",
-    },
-    {
-      nome: "Calabresa",
-      descricao: "Calabresa fatiada, cebola roxa e queijo mussarela.",
-      preco: "R$ 37,90",
-      imagem: "/imagens/cardapio/calabresa1.jpeg",
-    },
-    {
-      nome: "Marguerita",
-      descricao: "Mussarela, tomate, manjericão e azeite de oliva.",
-      preco: "R$ 35,90",
-      imagem: "/imagens/cardapio/marguerita1.jpeg",
-    },
-    {
-      nome: "Quatro Queijos",
-      descricao: "Mussarela, provolone, parmesão e gorgonzola.",
-      preco: "R$ 34,90",
-      imagem: "/imagens/cardapio/4queijos1.jpeg",
-    },
-  ];
-const pizzas_especiais = [
-    {
-      nome: "Pizza da Sally",
-      descricao: "Queijo cremoso, carne levemente temperada e um toque especial de molho barbecue",
-      preco: "R$ 45,90",
-      imagem: "/imagens/cardapio/sally.jpeg",
-    },
-    {
-      nome: "byroniana",
-      descricao: "Muçarela especial, calabresa artesanal, tiras crocantes de bacon, toque leve de catupiry e finalizada com orégano fresco e azeite premium",
-      preco: "R$ 45,90",
-      imagem: "/imagens/cardapio/byroniana.jpeg",
-    },
-    {
-      nome: "Pizza do Kody",
-      descricao: "Queijo, fatias de salmão defumado, cream cheese, cebolinha e um toque de shoyu.",
-      preco: "R$ 45,90",
-      imagem: "/imagens/cardapio/japonesa2.jpeg",
-    },
-];
-const pizzas_doces = [
-    {
-      nome: "Chororango",
-      descricao: "A combinação do amargo sutil do chocolate com a doçura e acidez dos morangos cria um equilíbrio perfeito, tornando cada mordida uma experiência deliciosa.",
-      preco: "R$ 38,90",
-      imagem: "/imagens/cardapio/doce1.jpeg",
-    },
-    {
-      nome: "Banana",
-      descricao: "A combinação da banana macia com a crocância da massa e o toque doce do chocolate é simplesmente irresistível.",
-      preco: "R$ 35,90",
-      imagem: "/imagens/cardapio/doce3.jpeg",
-    },
-    {
-      nome: "Choco branco",
-      descricao: "Raspas de chocolate branco, merengues pequenos e até mesmo morangos banhados em chocolate branco, criando uma textura e um sabor suaves e luxuosos.",
-      preco: "R$ 39,90",
-      imagem: "/imagens/cardapio/doce2.jpeg",
-    },
-];
 
   return (
     <main className="bg-orange-50 text-orange-600 font-bold min-h-screen py-12 px-4">
       <h1 className="text-4xl font-bold text-center mb-12">Nosso Cardápio</h1>
+
       <h2 className="text-2xl font-bold text-center mb-12">Pizzas Tradicionais</h2>
 
       <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-7xl mx-auto">
-        {pizzas_tradicionais.map((pizza, index) => (
+        {pizzas.slice(0,4).map((pizza, index) => (
           <div
             key={index}
             className="bg-white text-black rounded-2xl overflow-hidden shadow-xl hover:scale-105 transition-transform duration-300 border border-orange-700"
@@ -100,7 +36,7 @@ const pizzas_doces = [
       <h2 className="text-2xl font-bold text-center mb-12 mt-12">Pizzas Especiais</h2>
 
       <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-7xl mx-auto">
-        {pizzas_especiais.map((pizza, index) => (
+        {pizzas.slice(4,7).map((pizza, index) => (
           <div
             key={index}
             className="bg-white text-black rounded-2xl overflow-hidden shadow-xl hover:scale-105 transition-transform duration-300 border border-orange-700"
@@ -122,7 +58,7 @@ const pizzas_doces = [
       <h2 className="text-2xl font-bold text-center mb-12 mt-12">Pizzas Doces</h2>
 
       <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-7xl mx-auto">
-        {pizzas_doces.map((pizza, index) => (
+        {pizzas.slice(7,10).map((pizza, index) => (
           <div
             key={index}
             className="bg-white text-black rounded-2xl overflow-hidden shadow-xl hover:scale-105 transition-transform duration-300 border border-orange-700"
--- my-app/src/app/Carrinho/page.tsx
+++ my-app/src/app/Carrinho/page.tsx
@@ -1,45 +1,54 @@
 'use client';
+
+import { pizzas } from "@/Data/pizzas";
+
 export default function Carrinho() {
     return (
         <section className="bg-orange-50 py-12 px-6">
             <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-xl p-8 space-y-8">
-                <h2 className="text-3xl font-bold text-orange-600 text-center">🛒 Seu Carrinho</h2>
+                <h2 className="text-3xl font-bold text-orange-00 text-center">🛒 Seu Carrinho</h2>
 
-                <button className="bg-orange-500 hover:bg-orange-700 text-white rounded-md px-4 py-1 text-lg">
-                Limpar Carrinho
+                <button className="bg-orange-500 hover:bg-orange-700 text-white rounded-md px-4 py-1 text-sm">
+                    Limpar Carrinho
                 </button>
 
                 <div className="flex flex-col md:flex-row items-center gap-6 border-b pb-6">
-                    <img src="./imagens/cardapio/sally.jpeg" alt="Pizza da Sally" className="w-28 h-28 rounded-lg object-cover" />
+                    {pizzas.slice(0, 1).map((pizza, index) => (
+                        <div
+                        key={index}
+                        className="bg-white text-black rounded-2xl overflow-hidden shadow-xl hover:scale-105 transition-transform duration-300 border border-orange-700"
+                        >
+                            <img src={pizza.imagem} alt={pizza.nome} className="w-28 h-28 rounded-lg object-cover" />
 
-                    <div className="flex-1">
-                        <h3 className="text-2xl font-semibold text-gray-800">Pizza da Sally</h3>
-                        <p className="text-sm text-gray-600">Queijo cremoso, carne temperada e molho barbecue</p>
-                        <p className="text-orange-500 font-bold mt-2">R$ 39,90</p>
-                        <p className="text-sm text-gray-700">Quantidade: 1</p>
+                            <div className="flex-1">
+                                <h3 className="text-2xl font-semibold text-gray-800">{pizza.nome}</h3>
+                                <p className="text-sm text-gray-600">{pizza.descricao}</p>
+                                <p className="text-orange-500 font-bold mt-2">{pizza.preco}</p>
+                                <p className="text-sm text-gray-700">Quantidade: </p>
+                            </div>
+                            
+                            <div className="flex flex-col gap-2">
+                                <button className="bg-red-400 hover:bg-red-500 text-white rounded-md px-4 py-1 text-sm">
+                                    Remover
+                                </button>
+                                <button className="bg-green-400 hover:bg-green-500 text-white rounded-md px-4 py-1 text-sm">
+                                    Adicionar +
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                    <div className="text-right font-bold text-2xl text-orange-600 mt-6">
+                        Total: R$ 39,90
                     </div>
 
-                    <div className="flex flex-col gap-2">
-                        <button className="bg-red-400 hover:bg-red-500 text-white rounded-md px-4 py-1 text-sm">
-                            Remover
-                        </button>
-                        <button className="bg-green-400 hover:bg-green-500 text-white rounded-md px-4 py-1 text-sm">
-                            Adicionar 
+                    <div className="flex justify-end">
+                        <button className="bg-orange-500 hover:bg-orange-700 text-white rounded-md px-4 py-1 text-lg">
+                        Finalizar Compra
                         </button>
                     </div>
-                </div>
-
-                <div className="text-right font-bold text-2xl text-orange-600 mt-6">
-                    Total: R$ 39,90
-                </div>
-
-                <div className="flex justify-end">
-                    <button className="bg-orange-500 hover:bg-orange-700 text-white rounded-md px-4 py-1 text-lg">
-                    Finalizar Compra
-                    </button>
-                </div>
-
+                </div>  
             </div>
         </section>
+
     );
 }
